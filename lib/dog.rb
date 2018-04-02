09341cd7199a412cb0bac9b78616a9736490b42b
Laura Nadolski <laura.nadolski@gmail.com>
2018-04-02 02:05:51 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,13 +3,13 @@
 
 class Dog
   
-def name=(dogs_name)
-  @name = dogs_name
-end
-
-def name
-  @name
-end
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+ 
+  def name
+    @name
+  end
  
  def bark
    puts "woof!"
